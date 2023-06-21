2e9026c4c363eec6ee278309a288c0fafbfe9449
nguyendinhthoi <dinhthoi2411@gmail.com>
2023-06-21 21:43:40 +0700

    Done Homework Sort

--- src/ss14_sort/ShowInsertionSort.java
+++ src/ss14_sort/ShowInsertionSort.java
@@ -1,22 +1,37 @@
 package ss14_sort;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ShowInsertionSort {
     public static void main(String[] args) {
-        int[] array = {3, 2, 5, 4, 6, 1};
-        insertionSort(array);
+        System.out.println("Enter list size: ");
+        Scanner scanner = new Scanner(System.in);
+        int size = Integer.parseInt(scanner.nextLine());
+        int[] list = new int[size];
+        System.out.println("Ener " + list.length + " values: ");
+        for (int i = 0; i < list.length; i++) {
+            list[i] = Integer.parseInt(scanner.nextLine());
+        }
+        System.out.println("Your input list: ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + "\t");
+        }
+        System.out.println("\nBegin sort processing...");
+        insertionSort(list);
+        System.out.println("List after sort:  ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + "\t");
+        }
     }
 
     public static void insertionSort(int[] list) {
         int pos, x;
         for (int i = 1; i < list.length; i++) {
             x = list[i];
-            System.out.printf("Phần tử %d :",x);
             pos = i;
             while (pos > 0 && x < list[pos - 1]) {
                 list[pos] = list[pos - 1];
-                System.out.println(Arrays.toString(list));
                 pos--;
             }
             list[pos] = x;
